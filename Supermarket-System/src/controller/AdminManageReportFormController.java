c9efd9ac54bbbc7ad5556e2bfd1c72ce756e2354
kasun <https://github.com/GDSE-59-60-Project-Zone/Validations.git>
2022-05-20 13:45:33 +0530

    Add CashierSelectCustomerorOrderForm

--- Supermarket-System/src/controller/AdminManageReportFormController.java
+++ Supermarket-System/src/controller/AdminManageReportFormController.java
@@ -1,4 +1,20 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
+
 public class AdminManageReportFormController {
+    public AnchorPane AdminManageReportFormContext;
+
+    public void btnGoHomeOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage =(Stage) AdminManageReportFormContext.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/AdminselectReportorItem.fxml"))));
+        stage.centerOnScreen();
+    }
 }
