362128247c2f663b9f0f1a4385022fa7465ea840
kasun <https://github.com/GDSE-59-60-Project-Zone/Validations.git>
2022-05-20 09:19:20 +0530

    Add Project

---
+++ Supermarket-System/src/controller/MainFormController.java
@@ -0,0 +1,9 @@
+package controller;
+
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+
+public class MainFormController {
+    public JFXPasswordField pwdPassword;
+    public JFXTextField txtUserName;
+}
